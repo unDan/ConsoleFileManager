e539827699c7b074ac0ee6880b5b8b038cfb04e6
Daniil Zabaluev <alienterrorblade@gmail.com>
2021-04-17 14:18:17 +0300

    Add App Config class

---
+++ ConsoleFileManager/FileManager/AppConfig.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using ConsoleFileManager.Properties;
+
+namespace ConsoleFileManager.FileManager
+{
+ /// <summary>
+    /// Represents a data class that stores configuration of the application.
+    /// </summary>
+    public class AppConfig
+    {
+        /// <summary> The amount of file to show per page. </summary>
+        public int FilesPerPage { get; set; }
+        
+        /// <summary> The symbol to draw window borders with. </summary>
+        public string WindowBorder { get; set; }
+        
+        /// <summary> The background color of the console. </summary>
+        public string BackgroundColor { get; set; }
+        
+        /// <summary> The foreground color of the console. </summary>
+        public string ForegroundColor { get; set; }
+
+        
+        /// <summary>
+        /// Initialize a new instance of config with default settings.
+        /// </summary>
+        public AppConfig()
+        {
+            FilesPerPage = Settings.Default.filesPerPageDefault;
+            WindowBorder = Settings.Default.windowBorderSymbolDefault;
+            BackgroundColor = Settings.Default.backgroundColorDefault;
+            ForegroundColor = Settings.Default.foregroundColorDefault;
+        }
+
+
+        /// <summary>
+        /// Get the color corresponding specified color name. <para/>
+        /// If color name is incorrect then get the default color.
+        /// </summary>
+        /// <param name="colorName">The name of the color to get.</param>
+        /// <param name="defaultValue">The default value for color if</param>
+        /// <returns></returns>
+        public ConsoleColor ParseColor(string colorName, string defaultValue)
+        {
+            // try to get the color from config
+            var parsed = Enum.TryParse(colorName, true, out ConsoleColor color);
+
+            // if color in config could not be parsed then use default color
+            if (!parsed)
+            {
+                Enum.TryParse(defaultValue, true, out color);
+
+                // write the default color value to config property to make the config correct
+                if (BackgroundColor == colorName)
+                    BackgroundColor = defaultValue;
+                else if (ForegroundColor == colorName)
+                    ForegroundColor = defaultValue;
+            }
+
+            return color;
+        }
+    }
+}
