bcf62eb7a2e3e203181bfa91f0d3cb384feb1c9b
Daniil Zabaluev <alienterrorblade@gmail.com>
2021-04-18 18:15:05 +0300

    Add saving application state on console closing event

--- ConsoleFileManager/Program.cs
+++ ConsoleFileManager/Program.cs
@@ -1,7 +1,31 @@
-﻿namespace ConsoleFileManager
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleFileManager
 {
     internal class Program
     {
-        public static void Main(string[] args) { }
+        private delegate bool ConsoleCtrlHandler(int sig);
+
+        [DllImport("Kernel32")]
+        private static extern bool SetConsoleCtrlHandler(ConsoleCtrlHandler handler, bool add);
+        
+
+        public static void Main(string[] args)
+        {
+            var fm = FileManager.FileManager.GetInstance();
+            
+            // apply console closing event handler that will save the File Manager state
+            SetConsoleCtrlHandler(
+                s => {
+                    fm.SaveState();
+                    return false;
+                }, 
+                true
+            );
+            
+            fm.Loop();
+            
+            fm.SaveState();
+        }
     }
 }
